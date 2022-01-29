7058e6d71c133af2e1b31eefd06749b48d7bcd1a
takker99 <37929109+takker99@users.noreply.github.com>
2022-01-29 15:04:04 +0900

    :fire: いらない函数を消した

--- omnibox/hash.ts
+++
@@ -1,9 +0,0 @@
-/** 文字列をハッシュに変換 */
-export async function hash(text: string) {
-  const encoded = new TextEncoder().encode(text);
-  const digest = await crypto.subtle.digest("SHA-256", encoded);
-  const hash = Array.from(new Uint8Array(digest)).map((byte) =>
-    byte.toString(16).padStart(2, "0")
-  ).join("");
-  return parseInt(hash, 16) % 100;
-}
