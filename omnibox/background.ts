4e7f3454c31528d49064836e9b4b96038ba7e9bd
Motoso Inami <motosonomoto@gmail.com>
2022-02-15 23:03:39 +0900

    GoQuickにリクエストするときにcookieを送る

--- omnibox/background.ts
+++ omnibox/background.ts
@@ -83,7 +83,7 @@ browser.omnibox.onInputEntered.addListener(async (text) => {
   if (text.match(/^http/)) {
     browser.tabs.update({ url: text });
   } else {
-    const response = await fetch("https://goquick.org"); // GoQuick.orgユーザはGoQuick.orgを利用
+    const response = await fetch("https://goquick.org", { credentials: "include"}); // GoQuick.orgユーザはGoQuick.orgを利用
     const data = await response.text();
     browser.tabs.update({
       url: data.match("GoQuick Login")
